e8c832c9590e72208c590017932b663cd53f7665
Antonello Genuario <antonello_gen@hotmail.com>
2018-06-28 16:51:20 +0200

    Remuved unused code

--- Wisej.ASPNETMVCAuthentication/Platform/account.js
+++ Wisej.ASPNETMVCAuthentication/Platform/account.js
@@ -7,14 +7,6 @@
             var req = new qx.io.request.Xhr('/Account/SignOut');
             req.addListener('success', App.Page1.LogoutSuccess(), this);
             req.send();
-        },
-        checkLogin: function () {
-            /* sends a request to the account controller to logout the user */
-            var req = new qx.io.request.Xhr('/Account/CheckLogin');
-            req.addListener('success', function (e) {
-                window.location = "/Account/Login";
-            }, this);
-            req.send();
         }
     }
 });
